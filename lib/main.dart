70366042ecdfcdb96e8c6e1132f9c3485a677628
010473 <zhangxiaoting@htsc.com>
2019-08-26 00:07:46 +0800

    My AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,66 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MaterialApp(
+  title: 'Welcome to Flutter',
+  home: MyScaffold(),
+));
 
-class MyApp extends StatelessWidget {
+
+
+class MyAppBar extends StatelessWidget {
+
+  MyAppBar({this.title});
+  final Widget title;
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-      ),
-      // 由RandomWordsState管理，这使得用户在下一步中从一个屏幕导航到另一个屏幕时， 可以更轻松地更改导航栏中的的路由名称。
-      home: new RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  void _pushSaved() {
-    //将路由推入（push）到导航器的栈中，将会显示更新为该路由页面。 从导航器的栈中弹出（pop）路由，将显示返回到前一个路由。
-    //建立一个路由并将其推入到导航管理器栈中
-    //推入到导航管理器的栈
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final titles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          //ListTile.divideTiles() 在每个ListTile之间添加1像素的分割线
-          final divided =
-              ListTile.divideTiles(tiles: titles, context: context).toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text("Suggestions Saved"),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          alreadySaved ? _saved.remove(pair) : _saved.add(pair);
-        });
-      },
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
-        // 匿名回调函数
-        itemBuilder: (context, i) {
-          // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
-          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
-          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-          final index = i ~/ 2;
-          // 如果是建议列表中最后一个单词对
-          if (index >= _suggestions.length) {
-            // ...接着再生成10个单词对，然后添加到建议列表
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+    return new Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: new BoxDecoration(color: Colors.blue[500]),
+      child: new Row(
+        children: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            //null 会禁用 button
+            onPressed: null,
+          ),
+          // Expanded expands its child to fill the available space.
+          new Expanded(child: title),
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            //null 会禁用 button
+            onPressed: null,
+          ),
         ],
       ),
-      body: _buildSuggestions(),
     );
   }
 }
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Material 是UI呈现的“一张纸”
+    return new Material(
+      child: new Column(
+        children: <Widget>[
+          new MyAppBar(
+            title: new Text(
+              'Example text',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          //MyScaffold使用了一个Expanded来填充剩余的空间
+          new Expanded(
+            child: new Center(
+              child: new Text('Hello World'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
