d092ba6d124a4a446477f4fbe68583170455e740
010473 <zhangxiaoting@htsc.com>
2019-08-27 00:17:02 +0800

    Material App

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
-  title: 'Welcome to Flutter',
-  home: MyScaffold(),
-));
+      title: 'Welcome to Flutter',
+      home: TutorialHome(),
+    ));
 
-
-
-class MyAppBar extends StatelessWidget {
-
-  MyAppBar({this.title});
-  final Widget title;
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: new BoxDecoration(color: Colors.blue[500]),
-      child: new Row(
-        children: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            //null 会禁用 button
-            onPressed: null,
-          ),
-          // Expanded expands its child to fill the available space.
-          new Expanded(child: title),
-          new IconButton(
-            icon: new Icon(Icons.search),
-            tooltip: 'Search',
-            //null 会禁用 button
-            onPressed: null,
-          ),
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(icon: new Icon(Icons.menu), onPressed: null),
+        title: new Text('Example Text'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.search), onPressed: null)
         ],
+      ),
+      body: new Center(
+        child: new Text('Hello World'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: null,
+        child: new Icon(Icons.add),
       ),
     );
   }
 }
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Material 是UI呈现的“一张纸”
-    return new Material(
-      child: new Column(
-        children: <Widget>[
-          new MyAppBar(
-            title: new Text(
-              'Example text',
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
-          //MyScaffold使用了一个Expanded来填充剩余的空间
-          new Expanded(
-            child: new Center(
-              child: new Text('Hello World'),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
