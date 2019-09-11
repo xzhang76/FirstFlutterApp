a4dc3da4d83a62c046b66d88520f0ea28a493054
010473 <zhangxiaoting@htsc.com>
2019-09-11 19:51:44 +0800

    Scaffold and AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/Choice.dart';
+import 'package:myapp/ChoiceCard.dart';
 
-void main() => runApp(new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          leading: new IconButton(icon: new Icon(Icons.menu), onPressed: null),
-          title: new Text('Volume Control'),
-          actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.search), onPressed: null)
-          ],
-        ),
-        body: new MyStatefulWidget(),
-      ),
-    ));
+void main() => runApp(new BasicAppBarSample());
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key key}) : super(key: key);
+class BasicAppBarSample extends StatefulWidget {
+  const BasicAppBarSample({Key key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-    return new _MyStatefulWidgetState();
+    return new _BasicAppBarSampleState();
   }
 }
 
-int _count = 0;
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class _BasicAppBarSampleState extends State<BasicAppBarSample> {
+  Choice _selectedChoice = choices[0];
+
+  void _selectChoice(Choice choice) {
+    setState(() {
+      _selectedChoice = choice;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Text('Pressed button $_count times.'),
-      ),
-      backgroundColor: Colors.blueAccent.shade200,
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        setState(() {
-          _count++;
-        });
-      }, child: const Icon(Icons.add),),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(height: 50.0,),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: const Text('Basic AppBar'),
+            actions: <Widget>[
+              new IconButton(
+                  icon: new Icon(choices[0].icon),
+                  onPressed: () {
+                    _selectChoice(choices[0]);
+                  }),
+              new IconButton(
+                  icon: new Icon(choices[1].icon),
+                  onPressed: () {
+                    _selectChoice(choices[1]);
+                  }),
+              new PopupMenuButton<Choice>(
+                  itemBuilder: (BuildContext context) {
+                    return choices.skip(2).map((Choice choice) {
+                      return new PopupMenuItem<Choice>(
+                        child: new Text(choice.title),
+                        value: choice,
+                      );
+                    }).toList();
+                  },
+                  onSelected: _selectChoice),
+            ],
+          ),
+          body: new Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: new ChoiceCard(choice: _selectedChoice))),
     );
   }
 }
