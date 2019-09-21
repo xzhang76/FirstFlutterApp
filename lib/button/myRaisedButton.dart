fca64a7963dc49017e995a9c2333e5441d06e7d1
010473 <zhangxiaoting@htsc.com>
2019-09-22 01:31:51 +0800

    Chip, SimpleDialog and AlertDialog

--- lib/button/myRaisedButton.dart
+++ lib/button/myRaisedButton.dart
@@ -26,20 +26,24 @@ class _MyRaisedButtonState extends State<MyRaisedButton> {
             ),
             const SizedBox(height: 30),
             new RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  _askedToLead(context);
+                },
                 child: Text('enabled Button', style: TextStyle(fontSize: 10))),
             const SizedBox(height: 30),
             new RaisedButton(
-              onPressed: () {},
+              onPressed: () {
+                _neverSatisfied(context);
+              },
               textColor: Colors.white,
               padding: const EdgeInsets.all(0.0),
               child: new Container(
                   decoration: const BoxDecoration(
                       gradient: LinearGradient(colors: <Color>[
-                        Color(0xFF0D47A1),
-                        Color(0xFF1976D2),
-                        Color(0xFF42A5F5),
-                      ])),
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                    Color(0xFF42A5F5),
+                  ])),
                   padding: const EdgeInsets.all(15.0),
                   child: new Text('Gradient Button',
                       style: TextStyle(fontSize: 10))),
@@ -50,3 +54,66 @@ class _MyRaisedButtonState extends State<MyRaisedButton> {
     );
   }
 }
+
+enum Department { treasury, state }
+
+Future<void> _askedToLead(BuildContext context) async {
+  switch (await showDialog<Department>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Select assignment'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, Department.treasury);
+              },
+              child: const Text('Treasury department'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, Department.state);
+              },
+              child: const Text('State department'),
+            ),
+          ],
+        );
+      })) {
+    case Department.treasury:
+    // Let's go.
+    // ...
+      break;
+    case Department.state:
+    // ...
+      break;
+  }
+}
+
+Future<void> _neverSatisfied(BuildContext context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Rewind and remember'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('You will never be satisfied.'),
+              Text('You\’re like me. I’m never satisfied.'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Regret'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
--- lib/widgets/MyWidgetsHome.dart
+++ lib/widgets/MyWidgetsHome.dart
@@ -31,9 +31,102 @@ class _MyWidgetHomeState extends State<MyWidgetHome> {
                 labelText: 'Password',
               ),
             ),
+            Chip(
+              avatar: CircleAvatar(
+                backgroundColor: Colors.grey.shade800,
+                child: Text('AB'),
+              ),
+              label: Text('Aaron Burr'),
+            ),
+            new RaisedButton(
+                onPressed: () {
+                  _askedToLead(context);
+                },
+                child: Text('enabled Button', style: TextStyle(fontSize: 10))),
+            const SizedBox(height: 30),
+            new RaisedButton(
+              onPressed: () {
+                _neverSatisfied(context);
+              },
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0.0),
+              child: new Container(
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: <Color>[
+                        Color(0xFF0D47A1),
+                        Color(0xFF1976D2),
+                        Color(0xFF42A5F5),
+                      ])),
+                  padding: const EdgeInsets.all(15.0),
+                  child: new Text('Gradient Button',
+                      style: TextStyle(fontSize: 10))),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+enum Department { treasury, state }
+
+Future<void> _askedToLead(BuildContext context) async {
+  switch (await showDialog<Department>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Select assignment'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, Department.treasury);
+              },
+              child: const Text('Treasury department'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, Department.state);
+              },
+              child: const Text('State department'),
+            ),
+          ],
+        );
+      })) {
+    case Department.treasury:
+    // Let's go.
+    // ...
+      break;
+    case Department.state:
+    // ...
+      break;
+  }
+}
+
+Future<void> _neverSatisfied(BuildContext context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Rewind and remember'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('You will never be satisfied.'),
+              Text('You\’re like me. I’m never satisfied.'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Regret'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
