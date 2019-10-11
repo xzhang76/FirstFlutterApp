47c9e73e6455d4b9510e8357d480107108f2a976
010473 <zhangxiaoting@htsc.com>
2019-10-11 09:08:53 +0800

    TextField, 焦点

--- lib/input/MyInputWidget.dart
+++ lib/input/MyInputWidget.dart
@@ -13,6 +13,10 @@ class _MyInputWidgetState extends State<MyInputWidget> {
   String input = "";
   TextEditingController _nameController = TextEditingController();
 
+  FocusNode focusNode1 = new FocusNode();
+  FocusNode focusNode2 = new FocusNode();
+  FocusScopeNode focusScopeNode;
+
   @override
   void initState() {
     super.initState();
@@ -20,6 +24,12 @@ class _MyInputWidgetState extends State<MyInputWidget> {
       setState(() {
         input = _nameController.text;
       });
+    });
+    focusNode1.addListener(() {
+      print(focusNode1.hasFocus);
+    });
+    focusNode2.addListener(() {
+      print(focusNode2.hasFocus);
     });
   }
 
@@ -38,6 +48,7 @@ class _MyInputWidgetState extends State<MyInputWidget> {
                 hintText: "用户名或邮箱",
                 prefixIcon: Icon(Icons.person)),
             controller: _nameController,
+            focusNode: focusNode1,
           ),
           TextField(
             decoration: InputDecoration(
@@ -45,6 +56,7 @@ class _MyInputWidgetState extends State<MyInputWidget> {
                 hintText: "您的登录密码",
                 prefixIcon: Icon(Icons.lock)),
             obscureText: true,
+            focusNode: focusNode2,
           ),
           FlatButton.icon(
               onPressed: () {
@@ -54,6 +66,34 @@ class _MyInputWidgetState extends State<MyInputWidget> {
               },
               icon: Icon(Icons.info),
               label: Text('详情: $input')),
+          Builder(
+            builder: (ctx) {
+              return Column(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text("移动焦点"),
+                    onPressed: () {
+                      //将焦点从第一个TextField移到第二个TextField
+                      // 这是一种写法 FocusScope.of(context).requestFocus(focusNode2);
+                      // 这是第二种写法
+                      if (null == focusScopeNode) {
+                        focusScopeNode = FocusScope.of(context);
+                      }
+                      focusScopeNode.requestFocus(focusNode2);
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text("隐藏键盘"),
+                    onPressed: () {
+                      // 当所有编辑框都失去焦点时键盘就会收起
+                      focusNode1.unfocus();
+                      focusNode2.unfocus();
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
         ],
       ),
     );
