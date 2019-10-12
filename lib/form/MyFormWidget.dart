8d69d7c0d3d1b17edbdfd89de425b07f60b85b09
010473 <zhangxiaoting@htsc.com>
2019-10-12 13:46:26 +0800

    ProgressIndicator 进度色动画

--- lib/form/MyFormWidget.dart
+++ lib/form/MyFormWidget.dart
@@ -9,9 +9,28 @@ class MyFormRoute extends StatefulWidget {
   }
 }
 
-class _MyFormRouteState extends State<MyFormRoute> {
+class _MyFormRouteState extends State<MyFormRoute>
+    with SingleTickerProviderStateMixin {
   TextEditingController _unameController = new TextEditingController();
   TextEditingController _pwdController = new TextEditingController();
+
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    //动画执行时间3秒
+    _animationController =
+        new AnimationController(vsync: this, duration: Duration(seconds: 3));
+    _animationController.forward();
+    _animationController.addListener(() => setState(() => {}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,98 +38,106 @@ class _MyFormRouteState extends State<MyFormRoute> {
       appBar: AppBar(
         title: Text("Form Test"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-        child: Form(
-          autovalidate: false, //开启自动校验
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                  controller: _unameController,
-                  decoration: InputDecoration(
-                      labelText: "用户名",
-                      hintText: "用户名或邮箱",
-                      icon: Icon(Icons.person)),
-                  // 校验用户名
-                  validator: (v) {
-                    return v.trim().length > 0 ? null : "用户名不能为空";
-                  }),
-              TextFormField(
-                  controller: _pwdController,
-                  decoration: InputDecoration(
-                      labelText: "密码",
-                      hintText: "您的登录密码",
-                      icon: Icon(Icons.lock)),
-                  obscureText: true,
-                  //校验密码
-                  validator: (v) {
-                    return v.trim().length > 5 ? null : "密码不能少于6位";
-                  }),
-              // 登录按钮
-              Padding(
-                padding: const EdgeInsets.only(top: 28.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Builder(builder: (context) {
-                        return RaisedButton(
-                          padding: EdgeInsets.all(15.0),
-                          child: Text("登录"),
-                          color: Theme.of(context).primaryColor,
-                          textColor: Colors.white,
-                          onPressed: () {
-                            // 通过_Form.of(context) 获取FormState后，
-                            // 调用validate()方法校验用户名密码是否合法，校验
-                            // 通过后再提交数据。
-                            if (Form.of(context).validate()) {
-                              //验证通过提交数据
-                            }
-                          },
-                        );
-                      }),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+          child: Form(
+            autovalidate: false, //开启自动校验
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                    controller: _unameController,
+                    decoration: InputDecoration(
+                        labelText: "用户名",
+                        hintText: "用户名或邮箱",
+                        icon: Icon(Icons.person)),
+                    // 校验用户名
+                    validator: (v) {
+                      return v.trim().length > 0 ? null : "用户名不能为空";
+                    }),
+                TextFormField(
+                    controller: _pwdController,
+                    decoration: InputDecoration(
+                        labelText: "密码",
+                        hintText: "您的登录密码",
+                        icon: Icon(Icons.lock)),
+                    obscureText: true,
+                    //校验密码
+                    validator: (v) {
+                      return v.trim().length > 5 ? null : "密码不能少于6位";
+                    }),
+                // 登录按钮
+                Padding(
+                  padding: const EdgeInsets.only(top: 28.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Builder(builder: (context) {
+                          return RaisedButton(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text("登录"),
+                            color: Theme.of(context).primaryColor,
+                            textColor: Colors.white,
+                            onPressed: () {
+                              // 通过_Form.of(context) 获取FormState后，
+                              // 调用validate()方法校验用户名密码是否合法，校验
+                              // 通过后再提交数据。
+                              if (Form.of(context).validate()) {
+                                //验证通过提交数据
+                              }
+                            },
+                          );
+                        }),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              // 模糊进度条(会执行一个动画)
-              SizedBox(
-                height: 1.0,
-                child: LinearProgressIndicator(
+                SizedBox(
+                  height: 20.0,
+                ),
+                // 模糊进度条(会执行一个动画)
+                SizedBox(
+                  height: 1.0,
+                  child: LinearProgressIndicator(
+                    backgroundColor: Colors.grey[200],
+                    valueColor: AlwaysStoppedAnimation(Colors.blue),
+                  ),
+                ),
+//进度条显示50%
+                SizedBox(
+                  height: 20.0,
+                ),
+                LinearProgressIndicator(
+                  backgroundColor: Colors.grey[200],
+                  valueColor: AlwaysStoppedAnimation(Colors.blue),
+                  value: .5,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                // 模糊进度条(会执行一个旋转动画)
+                CircularProgressIndicator(
                   backgroundColor: Colors.grey[200],
                   valueColor: AlwaysStoppedAnimation(Colors.blue),
                 ),
-              ),
-//进度条显示50%
-              SizedBox(
-                height: 20.0,
-              ),
-              LinearProgressIndicator(
-                backgroundColor: Colors.grey[200],
-                valueColor: AlwaysStoppedAnimation(Colors.blue),
-                value: .5,
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              // 模糊进度条(会执行一个旋转动画)
-              CircularProgressIndicator(
-                backgroundColor: Colors.grey[200],
-                valueColor: AlwaysStoppedAnimation(Colors.blue),
-              ),
 //进度条显示50%，会显示一个半圆
-            SizedBox(
-              width: 50.0,
-              height: 50.0,
-              child: CircularProgressIndicator(
-                backgroundColor: Colors.grey[200],
-                valueColor: AlwaysStoppedAnimation(Colors.blue),
-                value: .5,
-              ),
+                SizedBox(
+                  width: 50.0,
+                  height: 50.0,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.grey[200],
+                    valueColor: AlwaysStoppedAnimation(Colors.blue),
+                    value: .5,
+                  ),
+                ),
+                LinearProgressIndicator(
+                  backgroundColor: Colors.grey[200],
+                  valueColor: ColorTween(begin: Colors.grey, end: Colors.blue)
+                      .animate(_animationController), // 从灰色变成蓝色
+                  value: _animationController.value,
+                ),
+              ],
             ),
-            ],
           ),
         ),
       ),
