98510e7ca566cef80f206af247d2df6a4a7bb690
010473 <zhangxiaoting@htsc.com>
2019-10-16 00:25:24 +0800

    Transform.translate Transform.rotate Transform.scale

--- lib/containerwidgets/MyContainerWidgetRoute.dart
+++ lib/containerwidgets/MyContainerWidgetRoute.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class MyContainerWidgetRoute extends StatefulWidget {
   const MyContainerWidgetRoute({Key key}) : super(key: key);
@@ -56,7 +57,7 @@ class _MyContainerWidgetRouteState extends State<MyContainerWidgetRoute> {
                 ),
                 Padding(
                   // 分别指定四个方向的补白
-                  padding: const EdgeInsets.fromLTRB(20.0, .0, 20.0, 20.0),
+                  padding: const EdgeInsets.fromLTRB(20.0, .0, 20.0, .0),
                   child: Text("Your friend"),
                 )
               ],
@@ -72,7 +73,7 @@ class _MyContainerWidgetRouteState extends State<MyContainerWidgetRoute> {
                 height: 5.0, //因为指定了最小高度为50,所以不生效
                 child: redBox),
           ),
-          SizedBox(width: 80.0, height: 80.0, child: redBox),
+          SizedBox(width: 50.0, height: 50.0, child: redBox),
           //多重限制,父子中相应数值较大的
           ConstrainedBox(
               constraints: BoxConstraints(minWidth: 200.0, minHeight: 20.0),
@@ -81,13 +82,13 @@ class _MyContainerWidgetRouteState extends State<MyContainerWidgetRoute> {
                 child: redBox,
               )),
           ConstrainedBox(
-              constraints: BoxConstraints(minWidth: 60.0, minHeight: 100.0), //父
+              constraints: BoxConstraints(minWidth: 60.0, minHeight: 80.0), //父
               //"去除"父级限制,但注意并非是真正的去除
               //父ConstrainedBox是作用于子UnconstrainedBox上，而redBox只受子ConstrainedBox限制
               child: UnconstrainedBox(
                 //“去除”父级限制
                 child: ConstrainedBox(
-                  constraints: BoxConstraints(minWidth: 90.0, minHeight: 20.0),
+                  constraints: BoxConstraints(minWidth: 90.0, minHeight: 5.0),
                   //子
                   child: redBox,
                 ),
@@ -106,12 +107,69 @@ class _MyContainerWidgetRouteState extends State<MyContainerWidgetRoute> {
                       blurRadius: 4.0)
                 ]),
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 18.0),
+              padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 5.0),
               child: Text(
                 "Login",
                 style: TextStyle(color: Colors.white),
               ),
             ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            color: Colors.deepOrange,
+            child: Transform(
+              alignment: Alignment.topRight, //相对于坐标系原点的对齐方式
+              transform: new Matrix4.skewY(0.3), //沿Y轴倾斜0.3弧度
+              child: new Container(
+                padding: const EdgeInsets.all(8.0),
+                color: Colors.blue,
+                child: const Text('Apartment for rent!'),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          DecoratedBox(
+            decoration:BoxDecoration(color: Colors.red),
+            //默认原点为左上角，左移20像素，向上平移5像素
+            child: Transform.translate(
+              offset: Offset(-20.0, -5.0),
+              child: Text("Hello world"),
+            ),
+          ),
+          SizedBox(height: 10),
+          DecoratedBox(
+            decoration:BoxDecoration(color: Colors.red),
+            child: Transform.rotate(
+              //旋转90度
+              angle:math.pi/2 ,
+              child: Text("world"),
+            ),
+          ),
+          SizedBox(height: 10),
+          DecoratedBox(
+              decoration:BoxDecoration(color: Colors.red),
+              child: Transform.scale(
+                  scale: 1.5, //放大到1.5倍
+                  child: Text("Hello world")
+              )
+          ),
+          SizedBox(height: 10),
+          // Transform的变换是应用在绘制阶段，而并不是应用在布局(layout)阶段，
+          // 所以无论对子组件应用何种变化，其占用空间的大小和在屏幕上的位置都是固定不变的，因为这些是在布局阶段就确定的。
+
+          //由于矩阵变化只会作用在绘制阶段，所以在某些场景下，在UI需要变化时，可以直接通过矩阵变化来达到视觉上的UI改变，
+          // 而不需要去重新触发build流程，这样会节省layout的开销，所以性能会比较好。
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              DecoratedBox(
+                  decoration:BoxDecoration(color: Colors.red),
+                  child: Transform.translate(offset: Offset(20.0, 5.0),
+                      child: Text("Hello world", style: TextStyle(color: Colors.blueGrey, fontSize: 15.0))
+                  )
+              ),
+              Text("你好", style: TextStyle(color: Colors.green, fontSize: 18.0),)
+            ],
           )
         ],
       ),
